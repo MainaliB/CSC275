cedbf6226b80b1f65802f855f89a0bd5fb974e82
Bibek Mainali <bibek.mainali20@gmail.com>
2022-04-12 22:23:06 -0400

    I/O Operations Java

---
+++ Problem3.java
@@ -0,0 +1,41 @@
+
+import java.io.File;
+import java.util.Scanner;
+
+/*
+ There is a file attached that contains multiple lines. On each line is a word (or at least close to it)
+
+Your task: Prompt the user for a word. Read each line of the file until you either 
+find the word the user entered, or reach the end of the file. If you find the
+word print out "Word Found!", if the word was not found in the list print 
+out "Word Not Found!"
+ */
+public class Problem3 {
+    public static void main (String[] args) throws Exception{
+        File file = new File("Problem3.txt");
+            
+    Scanner input = new Scanner(file);
+    System.out.println("Please enter a word: ");
+    Scanner inputFromUser = new Scanner(System.in);
+    String userInput = inputFromUser.next();
+    String valueToPrint="";
+    while(input.hasNext()){
+        String value = input.next();
+        if (userInput.equalsIgnoreCase(value)){
+            valueToPrint = "Word Found!";
+            
+            break;
+        
+        }
+        else{
+            valueToPrint = "Word Not Found!";
+        }
+        
+    }
+    System.out.println(valueToPrint);
+    input.close();
+    
+    }
+    
+    
+}
