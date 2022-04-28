b95b838c057ada61b5641990ee3ec42987b43add
Bibek Mainali <bibek.mainali20@gmail.com>
2022-04-27 23:04:06 -0400

    Work in progress, learning recursion

---
+++ Summation.java
@@ -0,0 +1,10 @@
+/*
+Write a program that that recursively traverses an array of integers and returns
+the total sum of all integers in the list.
+ */
+public class Summation {
+    public int recursiveSummation(int [] myArray){
+    
+    }
+    
+}
