26fc5c1a5add1f9f9a831849b8d5250337123d8d
Bibek Mainali <bibek.mainali20@gmail.com>
2022-04-28 23:29:34 -0400

    Recursive Method Implementation-Completed

--- ReverseString.java
+++ ReverseString.java
@@ -3,9 +3,18 @@
 reverse a given string.
  */
 public class ReverseString {
+    
+    
     public void reverse(String s){
-    
-    
+        if (s == null || s.length() <=1){
+            System.out.println(s);
+        }
+        else {
+            System.out.print(s.charAt(s.length()-1));
+            reverse(s.substring(0,s.length()-1));
+        }
+            
+      
     }
     
 }
