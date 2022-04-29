a632c7206d584b0ce97893775b44429d0f29cd9d
Bibek Mainali <bibek.mainali20@gmail.com>
2022-04-28 23:30:02 -0400

    Recursive Method Implementation-Completed

--- ReverseArray.java
+++ ReverseArray.java
@@ -3,8 +3,29 @@ Given an array of integers use recursion to reverse the entire list.
 You should only use recursion and you should not create a second array.
  */
 public class ReverseArray {
+    
     public void reverseArray(int[] myArray){
+        if (myArray.length == 0)
+            return;
+        
+        else{
+            reverse(myArray, 0, myArray.length-1);
+        }
+        for (int i = 0; i < myArray.length; i++){
+            System.out.println(myArray[i]);
+        }        
         
     }
+    public void reverse(int [] a, int start, int end){
+        if (start>= end)
+            return;
+        
+        int temp = a[start];
+        a[start]= a[end];
+        a[end]=temp;
+        reverse(a, start+1, end-1);
+    }
     
+        
+
 }
