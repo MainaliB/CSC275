2585bdb26dbab0c1d2382edd3ede97ba691fda81
Bibek Mainali <bibek.mainali20@gmail.com>
2022-04-29 22:19:26 -0400

    Implementing Recersive Methods WIP

---
+++ SplitArrays.java
@@ -0,0 +1,16 @@
+/*
+ Given one array that is filled with random integers, split this large array 
+into two equal sized smaller arrays. Use only recursion to perform this task. 
+All items at even indices in the large array should be put in array one, and 
+all items at the odd indices in the large array should be put in array two.
+ */
+public class SplitArrays {
+    
+    
+    public void splitArrays(int[] largeArray, int[] arrayOne, int[] arrayTwo){
+        
+        
+    }
+
+    
+}
