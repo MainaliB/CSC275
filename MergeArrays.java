6b8753e6f7decb656396ea20ebb9f8125012e775
Bibek Mainali <bibek.mainali20@gmail.com>
2022-04-29 22:17:01 -0400

    Working with Recursive Methods

--- MergeArrays.java
+++ MergeArrays.java
@@ -5,8 +5,25 @@ even indices (0,2,4 ...) and items from the second array in odd indices (1,3,5
 ...). Only use recursion to complete this task.
  */
 public class MergeArrays {
+    
     public void mergeArrays(int[] arrayOne, int[] arrayTwo, int[] mergedArray){
+        merge(arrayOne, 0, mergedArray, 0);
+        merge(arrayTwo, 0, mergedArray, 1);
+        
     
     
+    }
+    public int [] merge(int[] arrayOne, int startArrayOne, int[] mergedArray, int startMergedArray){
+        if (arrayOne.length <= startArrayOne ){
+            return mergedArray;
+        }
+        else
+            
+            mergedArray[startMergedArray]= arrayOne[startArrayOne];
+            startArrayOne++;
+            startMergedArray = startMergedArray + 2;
+            merge(arrayOne, startArrayOne, mergedArray, startMergedArray);
+            return mergedArray;
+        
     }
 }
