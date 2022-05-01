6a11a121f9cc59651adbaaa3f6fe42d7f250b78c
Bibek Mainali <bibek.mainali20@gmail.com>
2022-04-30 22:03:13 -0400

    Recursion Implementation Work in Progress

--- SplitArrays.java
+++ SplitArrays.java
@@ -1,6 +1,6 @@
 /*
  Given one array that is filled with random integers, split this large array 
-into two equal sized smaller arrays. Use only recursion to perform this task. 
+iinto two equal sized smaller arrays. Use only recursion to perform this task. 
 All items at even indices in the large array should be put in array one, and 
 all items at the odd indices in the large array should be put in array two.
  */
@@ -10,6 +10,16 @@ public class SplitArrays {
     public void splitArrays(int[] largeArray, int[] arrayOne, int[] arrayTwo){
         
         
+    }
+    
+    public void split(int[] arrayOne, int startArrayOne, int [] largeArray, int startLargeArray ){
+        if (startArrayOne >= arrayOne.length)
+            return;
+        else
+            arrayOne[startArrayOne] = largeArray[startLargeArray];
+            startArrayOne ++;
+            startLargeArray = startLargeArray + 2;
+            split(arrayOne, startArrayOne, largeArray, startLargeArray);
     }
 
     
