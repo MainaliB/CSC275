c719ded532b82a45bf917a820c26627ff1f7d3e5
Bibek Mainali <bibek.mainali20@gmail.com>
2022-05-02 23:18:20 -0400

    Completed

--- EncodeVowels.java
+++ EncodeVowels.java
@@ -6,14 +6,21 @@ public class EncodeVowels {
     
     
     public String encodeVowels(String s){
-        char [] vowels = {'A','E','I','O','U'};
-        
-        
-        
-    }
-    
-    public String encode (String s){
-    
-    
+        // Base Case
+        if(s.length()==0){
+            return "";
+        } 
+        else {
+            char first = s.charAt(0);
+            
+            if(first=='A' || first=='E' || first=='I' || first=='O' || first=='U') {
+                
+                return '@' + encodeVowels(s.substring(1, s.length()));
+            } 
+            else { // if the first letter is not a vowel
+                // recursive call the function, and return the result
+                return first + encodeVowels(s.substring(1, s.length()));
+            }
+        }
     }
 }
