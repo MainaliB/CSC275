2851c97d7645da0302265ff0fb52991c49d9153f
Bibek Mainali <bibek.mainali20@gmail.com>
2022-05-04 23:17:29 -0400

    work in progress

---
+++ Cat.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bibekmainali
+ */
+public class Cat extends Pet{
+    private String breed;
+    private String gender;
+    
+    
+}
